9ea889e7664f10c19c13b63c89a4e2da955ad919
oezingle <oezingle@gmail.com>
2021-03-19 20:06:47 -0700

    fixed permission chat types

--- src/main/java/com/paratopiamc/bungee_towny/chat/mutecommand/MuteCommandExecutor.java
+++ src/main/java/com/paratopiamc/bungee_towny/chat/mutecommand/MuteCommandExecutor.java
@@ -21,6 +21,7 @@ public class MuteCommandExecutor implements CommandExecutor {
                 //mute the player
 
                 //notify them
+                player.sendMessage(Translation);
 
                 //return true
                 return true;
